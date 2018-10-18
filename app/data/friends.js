e5cf681850d3142efb397df72346ab9ecb3197ec
Sarah Subero <ssubero06@gmail.com>
2018-10-18 16:17:42 -0400

    App should work

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+	{
+        name: "Fran Tick",
+        photo: "https://i.imgur.com/QPZoZUP.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+        name: "Hal E. Luya",
+        photo: "https://i.imgur.com/6CnJhJC.png",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+        name: "Hayden Seek",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg0voysBa_bynE7Y09R7mduVtGkegId3JknxRJT2vgwTnL7vXS",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+        name: "Rudy Nuff",
+        photo:"https://i.imgur.com/hSy6mOO.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+        name: "Nick Ovtime",
+        photo: "https://i.imgur.com/nXxIp7F.png",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+        name: "Jack Dupp",
+        photo: "https://i.imgur.com/RxwtAgo.png",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
